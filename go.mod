bca973d3b93c35de867462292a48147eae83d818
zen <mbp>
2025-04-19 19:38:16 +0800

    feat(storage): 添加MySQL存储支持并更新依赖
    添加了MySQL存储支持，包括配置连接、创建数据库和表的功能。同时更新了go.mod和go.sum文件，添加了必要的依赖项，如github.com/go-sql-driver/mysql和xorm.io/xorm。

--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ go 1.24.2
 require (
 	github.com/gin-contrib/timeout v1.0.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-sql-driver/mysql v1.9.2
 	github.com/zhangyiming748/basicGin v0.0.0-20241103013125-23661cf3b137
 	github.com/zhangyiming748/lumberjack v0.0.1
+	xorm.io/xorm v1.3.9
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.12.6 // indirect
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -20,6 +23,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -27,6 +31,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.12.0 // indirect
@@ -36,4 +41,5 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 )
---
+++ storage/mysql.go
@@ -0,0 +1,91 @@
+package mysql
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
+)
+
+var (
+	useMysql bool
+	engine   *xorm.Engine
+)
+
+func SetMysql() {
+	var err error
+	user := os.Getenv("MYSQL_USER")
+	if user == "" {
+		user = "root"
+	}
+	password := os.Getenv("MYSQL_PASSWORD")
+	if password == ""  {
+		password = "163453"
+	}
+	host := os.Getenv("MYSQL_HOST")
+	if host == "" {
+		host = "192.168.2.10"
+	}
+	port := os.Getenv("MYSQL_PORT")
+	if port == "" {
+		port = "3306"
+	}
+	log.Printf("连接mysql使用的各种参数-> %v:%v@%v:%v\n", user, password, host, port)
+	// 先连接到 MySQL 服务器（不指定数据库）
+	rootDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8", user, password, host, port)
+	log.Printf("第一次连接数据库的参数%s\n", rootDSN)
+	tempEngine, err := xorm.NewEngine("mysql", rootDSN)
+	if err != nil {
+		log.Printf("连接MySQL服务器失败: %v\n", err)
+		useMysql = false
+		return
+	}
+	// 修改这里：使用 tempEngine 而不是 engine
+	if err = tempEngine.Ping(); err != nil {
+		log.Printf("连接数据库失败: %v\n", err)
+		useMysql = false
+		return
+	} else {
+		log.Printf("成功Ping到数据库\n")
+		useMysql = true
+	}
+	// 检查数据库是否存在
+	rows, err := tempEngine.QueryString("SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'translate'")
+	if err != nil {
+		log.Printf("查询数据库失败: %v\n", err)
+		useMysql = false
+		return
+	}
+	// 如果数据库不存在，创建它
+	if len(rows) == 0 {
+		_, err = tempEngine.Exec("CREATE DATABASE `translate` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'")
+		if err != nil {
+			log.Printf("创建数据库失败: %v\n", err)
+			useMysql = false
+			return
+		}
+		log.Println("成功创建数据库 translate")
+	}
+	// 关闭临时连接
+	tempEngine.Close()
+	// 连接到 tdl 数据库
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/tdl?charset=utf8mb4", user, password, host, port)
+	log.Printf("第二次连接数据库的参数%s\n", dataSourceName)
+	engine, err = xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		log.Printf("连接tdl数据库失败: %v\n", err)
+		useMysql = false
+		return
+	}
+	log.Println("成功连接到数据库")
+}
+
+func GetMysql() *xorm.Engine {
+	return engine
+}
+
+func UseMysql() bool {
+	return useMysql
+}
